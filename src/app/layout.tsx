56aec388686f712ba4774bed4a23c0476ec6ddd3
rajivlodhia <r_lodhia@hotmail.com>
2024-01-17 21:25:47 +0000

    Basic header layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,7 +14,23 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={cn(inter.className, 'transition')}>
+                <header className="py-6 max-w-7xl mx-auto flex justify-between">
+                    <Link href="/">Rajiv Lodhia</Link>
+                    <nav className="flex flex-row gap-5">
+                        <Link href="#about" className="hover:opacity-80">
+                            About
+                        </Link>
+                        <Link href="#contact" className="hover:opacity-80">
+                            Contact Me
+                        </Link>
+                        <Link href="#projects" className="hover:opacity-80">
+                            Projects
+                        </Link>
+                    </nav>
+                </header>
+                {children}
+            </body>
         </html>
     )
 }
