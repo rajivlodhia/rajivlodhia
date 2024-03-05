9a531c84e569161fcc03e90300d5bf4c47504cfd
rajivlodhia <r_lodhia@hotmail.com>
2024-03-05 20:59:15 +0000

    Add background gradient animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { League_Spartan } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 import _Link from '@/components/ui/_Link'
+import { BackgroundGradientAnimation } from '@/components/ui/background-gradient-animation'
 
 const font = League_Spartan({
     subsets: ['latin'],
@@ -19,6 +20,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <html lang="en">
             <body className={cn(font.className, 'min-h-screen bg-slate-900')} style={{ scrollSnapType: 'y mandatory' }}>
                 {children}
+                <BackgroundGradientAnimation
+                    containerClassName="fixed -z-50"
+                    gradientBackgroundStart="rgb(2, 6, 23)"
+                    gradientBackgroundEnd="rgb(15, 23, 42)"
+                    firstColor="8, 15, 31"
+                    secondColor="18, 32, 64"
+                    thirdColor="17, 52, 94"
+                    fourthColor="51, 65, 85"
+                    fifthColor="31, 53, 84"
+                    pointerColor="24, 48, 82"
+                />
             </body>
         </html>
     )
