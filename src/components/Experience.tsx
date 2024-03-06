95381b58f71df419698ce6045a6f23fbd7f00bdc
rajivlodhia <r_lodhia@hotmail.com>
2024-03-06 22:01:46 +0000

    Clean up Experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -6,20 +6,29 @@ type Props = {}
 
 const Experience: NextComponentType<NextPageContext, object, Props> = (props: Props) => {
     return (
-        <>
-            <Heading2>Experience</Heading2>
-            <div className="flex gap-6 sm:gap-10 justify-center flex-wrap max-w-3xl mx-auto">
-                <ExperienceLogo src="/experience/css-logo.png" alt="CSS Logo" title="CSS" />
-                <ExperienceLogo src="/experience/git-logo.png" alt="CSS Logo" title="GIT" />
+        <div className="flex flex-col md:flex-row my-auto items-center gap-4">
+            <span className="space-y-3 md:max-w-lg">
+                <Heading2>Experience</Heading2>
+                <p>A diverse set of technical experience in both frontend and backend.</p>
+                <p>
+                    My goal is to keep expanding my knowledge as technology changes, and grow with the industry. With
+                    years of experience writing in PHP for WordPress and Drupal projects, I decided to expand my
+                    skillset and learn fullstack development with React and NextJS.
+                </p>
+            </span>
+            <div className="flex gap-6 sm:gap-10 justify-center flex-wrap max-w-3xl ml-auto">
                 <ExperienceLogo src="/experience/nextjs-logo.png" alt="CSS Logo" title="NextJS" />
-                <ExperienceLogo src="/experience/php-logo.png" alt="CSS Logo" title="PHP" />
                 <ExperienceLogo src="/experience/react-logo.webp" alt="CSS Logo" title="React" />
-                <ExperienceLogo src="/experience/tailwind-logo.png" alt="CSS Logo" title="Tailwind CSS" />
                 <ExperienceLogo src="/experience/typescript-logo.png" alt="CSS Logo" title="TypeScript" />
-                <ExperienceLogo src="/experience/vscode-logo.png" alt="CSS Logo" title="VS Code" />
+                <ExperienceLogo src="/experience/php-logo.png" alt="CSS Logo" title="PHP" />
+                <ExperienceLogo src="/experience/css-logo.png" alt="CSS Logo" title="CSS" />
+                <ExperienceLogo src="/experience/tailwind-logo.png" alt="CSS Logo" title="Tailwind CSS" />
                 <ExperienceLogo src="/experience/wordpress-logo.webp" alt="CSS Logo" title="WordPress" />
+                <ExperienceLogo src="/experience/drupal-logo.webp" alt="Drupal Logo" title="Drupal" />
+                <ExperienceLogo src="/experience/git-logo.png" alt="CSS Logo" title="GIT" />
+                <ExperienceLogo src="/experience/vscode-logo.png" alt="CSS Logo" title="VS Code" />
             </div>
-        </>
+        </div>
     )
 }
 
