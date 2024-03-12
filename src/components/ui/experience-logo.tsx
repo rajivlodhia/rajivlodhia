80d3575ec5297c8c826b597d50162e29b66458d9
rajivlodhia <r_lodhia@hotmail.com>
2024-03-12 21:34:46 +0000

    Add hover effect to experience logos

--- src/components/ui/experience-logo.tsx
+++ src/components/ui/experience-logo.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const ExperienceLogo: NextComponentType<NextPageContext, object, Props> = (props: Props) => {
     return (
-        <span className="flex flex-col items-center">
+        <span className="flex flex-col items-center group/experience-logo">
             <Image
                 className="grayscale brightness-0 invert object-contain w-auto h-24 max-w-16 sm:max-w-24 sm:h-32"
                 src={props.src}
@@ -17,7 +17,7 @@ const ExperienceLogo: NextComponentType<NextPageContext, object, Props> = (props
                 width={96}
                 height={96}
             />
-            <p>{props.title}</p>
+            <p className="group-hover/experience-logo:text-rose-300 transition-colors">{props.title}</p>
         </span>
     )
 }
