5146ae78d9d5ffdc9d7c52652e95f7a31fe8afec
rajivlodhia <r_lodhia@hotmail.com>
2024-03-12 20:32:54 +0000

    Add Contact content + an animated underline hover effect

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,28 @@ const Contact: NextComponentType<NextPageContext> = () => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 my-auto items-center">
             <Heading2>Get In Touch</Heading2>
-            <p></p>
+            <article>
+                <p className="text-lg mb-8">
+                    Got a new opportunity or exciting project?
+                    <br />
+                    Drop me an email or find me on{' '}
+                    <a
+                        className="text-rose-300 animated-underline inline after:h-[2px]"
+                        href="https://www.linkedin.com/in/rajiv-lodhia"
+                        target="_blank"
+                    >
+                        LinkedIn
+                    </a>
+                    .
+                </p>
+
+                <a
+                    href="mailto:r_lodhia@hotmail.com"
+                    className="text-rose-300 relative font-bold text-4xl w-fit block animated-underline"
+                >
+                    r_lodhia@hotmail.com
+                </a>
+            </article>
         </div>
     )
 }
