41afabbba9bbf9c21efa5b1e1419640b9c466869
rajivlodhia <r_lodhia@hotmail.com>
2024-03-12 21:28:01 +0000

    Simple smooth anchor scrolling

--- src/components/ui/nav-link.tsx
+++ src/components/ui/nav-link.tsx
@@ -1,13 +1,34 @@
+'use client'
+
 import { cn } from '@/lib/utils'
 import Link, { LinkProps } from 'next/link'
 
 type Props = {
     children: React.ReactNode
+    onClick?: () => void
     className?: string
 } & LinkProps
 
 const NavLink = (props: Props) => {
-    return <Link className={cn('hover:text-rose-300 transition-colors', props.className)} {...props} />
+    const handleScrollToAnchor = (e: React.MouseEvent<HTMLAnchorElement>) => {
+        e.preventDefault()
+        props.onClick?.()
+
+        const href = e.currentTarget.getAttribute('href')
+        if (!href) return
+        const element = document.querySelector(href)
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth' })
+        }
+    }
+
+    return (
+        <Link
+            className={cn('hover:text-rose-300 transition-colors', props.className)}
+            onClick={handleScrollToAnchor}
+            {...props}
+        />
+    )
 }
 
 export default NavLink
