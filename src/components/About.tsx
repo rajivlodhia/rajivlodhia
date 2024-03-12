e1eaf5b797028cd509a5584f1db6ee2e5f7c3cd8
rajivlodhia <r_lodhia@hotmail.com>
2024-03-12 17:38:57 +0000

    Add more About content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,11 +5,19 @@ const About: NextComponentType<NextPageContext> = () => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 my-auto items-center">
             <Heading2>About Me</Heading2>
-            <p className="text-lg">
-                I am a full-stack web developer with a passion for creating beautiful and functional user experiences. I
-                have a strong background in design and user experience, and I am always looking for new challenges and
-                opportunities to learn and grow.
-            </p>
+            <article className="text-lg space-y-4">
+                <p>
+                    I am a software engineer with a strong background in web development. My passion for software
+                    started way back in school when decided to try modding games, and I&apos;ve loved it ever since.
+                    Fast forward to today and I still try my best to keep myself informed on modern software-related
+                    trends and self-teach in my spare time.
+                </p>
+                <p>
+                    I&apos;m always seeking new opportunities and challenges - eager to contribute to ambitious projects
+                    alongside innovative, creative minds. In doing so, I hope use my unique skillset to make the world a
+                    better place.
+                </p>
+            </article>
         </div>
     )
 }
